204e22c89cd72c7fff83e43137083b06c758d0bf
mukundan314 <mukundan314@gmail.com>
2018-06-22 23:20:54 +0530

    Created jacks/Deck.py

---
+++ jacks/deck.py
@@ -0,0 +1,26 @@
+from .card import Card
+
+import random
+
+class Deck:
+    def __init__(self, shuffle=True):
+        self.reset(shuffle)
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def draw(self, n=1):
+        cards = self.deck[:n]
+        self.deck = self.deck[n:]
+        return cards
+
+    def reset(self, shuffle=False):
+        self.deck = []
+        for suit in ['s', 'h', 'd', 'c']:
+            for rank in Card.STR_RANKS:
+                self.deck.append(Card(rank+suit))
+        if shuffle:
+            self.shuffle()
+
+    def __str__(self):
+        return str(self.deck)
