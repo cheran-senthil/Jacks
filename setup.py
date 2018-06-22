b59ab3c3f643bb858ed5ed8b4c5f0937864cf114
mukundan314 <mukundan314@gmail.com>
2018-06-22 17:16:34 +0530

    add setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Jacks',
+      version='0.01',
+      description='Basic Commands for Poker',
+      url='https://github.com/Cheran-Senthil/Jacks',
+      author='Cheran and Mukundan',
+      license='Apache License 2.0',
+      packages=['jacks'])
