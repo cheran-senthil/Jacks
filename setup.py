24500f45d6d992e0af5da527f89e0a4247717d99
mukundan314 <mukundan314@gmail.com>
2018-06-23 09:15:18 +0530

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(name='Jacks',
-      version='0.01',
+      version='0.0.1',
       description='Basic Commands for Poker',
+      long_description=readme(),
       url='https://github.com/Cheran-Senthil/Jacks',
       author='Cheran and Mukundan',
       license='Apache License 2.0',
-      packages=['jacks'])
+      packages=['jacks'],
+      install_requires=[
+          'six',
+      ])
